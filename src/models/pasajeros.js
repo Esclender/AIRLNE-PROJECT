4418a314ec0f3375785fbfac93cc5f8a8757fd2d
Haslon <76234082@certus.edu.pe>
2023-06-02 09:07:02 -0500

    Dev:PasajerosModel
    Arcentales

---
+++ src/models/pasajeros.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const pasajeroSchema = mongoose.Schema({
+  name:{
+    type:String,
+    required: true
+  },
+  LastName:{
+    type:String,
+    required: true
+  },
+  Age:{
+    type:Number,
+    required: true
+  },
+  Passport_N:{
+    type:Number,
+    required: true
+  },
+  Destination:{
+    type:String,
+    required: true
+  }
+})
+
+const pasajeros = mongoose.model("pasajeros", pasajeroSchema)
+
+export default pasajeros
