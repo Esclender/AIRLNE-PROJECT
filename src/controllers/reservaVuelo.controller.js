678791386cf0553cb7b59c212791f33d46d462cb
luis2469 <72839383@certus.edu.pe>
2023-06-03 23:26:26 -0500

    dev: reservaVuelo controller added

---
+++ src/controllers/reservaVuelo.controller.js
@@ -0,0 +1,36 @@
+import reservaVueloServices from "../services/reservaVuelos.service.js"
+
+async function getReservaVuelos(req,res){
+  try {
+
+    const rst = await reservaVueloServices.getReservaVuelo()
+    return await res.json(rst)
+
+  } catch (error) {
+    res.status(404).json({
+      message:error
+    })
+  }
+}
+
+async function postReservaVuelos(req,res){
+  try {
+    
+    const rst = await reservaVueloServices.postReservaVuelo(req.body)
+    return await res.status(201).json({
+      message:"Nueva reserva registrada",
+      data:rst
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      message:error
+    })
+  }
+}
+
+
+export default {
+  getReservaVuelos,
+  postReservaVuelos
+}
