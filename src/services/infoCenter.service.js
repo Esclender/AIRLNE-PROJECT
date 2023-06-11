c72bda8170c6c52ee15dc29e4e7cfff8151bfb53
EduardoJair <112762817+EduardoJair@users.noreply.github.com>
2023-06-11 15:03:12 -0500

    dev: infoCenter service updated

--- src/services/infoCenter.service.js
+++ src/services/infoCenter.service.js
@@ -2,24 +2,37 @@ import infoCenterSchema from "../models/infoCenter.js"
 import db from "../database/project.module.js"
 import BaseException from "../exceptions/baseExceptions.module.js"
 
+
 const model = new db(infoCenterSchema)
+
 
 async function getInfoCenter(){
   const rst = await model.get()
-  const mapped = await rst.map(schema => schema.toJson(schema))
+  const claimsArray = rst.filter(cla => !cla._doc.allClaims)
+  const mapped = await claimsArray.map(schema => schema.toJson(schema))
   return await mapped
 }
+
+
+async function getInfoCenterClaims(){
+  const rst = await infoCenterSchema.findOne({"allclaims":[]})
+  const {allClaims} = rst._doc
+  return await allClaims
+}
+
 
 async function postInfoCenter(body){
   const rst = await model.post(body)
   return await rst.toJson(rst)
 }
 
+
 async function putInfoCenter(id, body){
   const rst = await model.put(id,body)
-  if(!rst.modifiedCount) throw new BaseException("Center not found", 404);
+  if(!rst.modifiedCount) throw new BaseException("Center not found", 404)
   return rst
 }
+
 
 async function deleteInfoCenter(id){
   const rst = await model.delete(id)
@@ -27,9 +40,11 @@ async function deleteInfoCenter(id){
   return rst
 }
 
+
 export default {
   getInfoCenter,
   postInfoCenter,
   putInfoCenter,
-  deleteInfoCenter
+  deleteInfoCenter,
+  getInfoCenterClaims
 }
