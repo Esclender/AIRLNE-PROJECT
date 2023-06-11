6105a4dde6b2d2f6c5c87c11606fda4897e65d8d
richz <richarmiguelrodriguez@gmail.com>
2023-06-11 15:24:28 -0500

    dev: infoCenter model updated

--- src/models/infoCenter.js
+++ src/models/infoCenter.js
@@ -1,22 +1,36 @@
 import mongoose from "mongoose";
 
+
 const infoCenterSchema = mongoose.Schema({
-    contactNumbers:{
-        type:Array,
-        required: true
-    }
-})
+  userData:{
+    type:Object,
+    required: true
+  },
+  comment:{
+    type: String,
+    required: true
+  },
+  claimType:{
+    type:String,
+    required: true
+  }
+}, { timestamps: true })
+
+
+
 
 
 
 infoCenterSchema.methods = {
   toJson: (schema) => {
-    const {__v, _id, ...vuelo} = schema.toObject()
-    vuelo.id = _id
-    return vuelo
+    const {__v, _id, ...info} = schema.toObject()
+    info.id = _id
+    return info
   }
 }
 
-const infoCenter = mongoose.model("infocenter", infoCenterSchema)
+
+const infoCenter = mongoose.model("infocenters", infoCenterSchema)
+
 
 export default infoCenter
