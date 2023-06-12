6b3ce2e9f05338046350d90a84e3a8c0d6aa220b
Esclender-Lugo <e00181703@certus.edu.pe>
2023-06-11 20:17:17 -0500

    hotfix

---
+++ src/middleware/validateClientJwt.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import regularClientSchema from "../models/regularClient.js"
+
+async function verifyJwt(req,res,next) {
+  let token = req.header("X-API-KEY")
+
+
+  if (!token) {
+    return res.status(401).send("A client token is required for authentication");
+  }
+
+  try {
+    const {username} = jwt.verify(token, process.env.TOKEN_KEY);
+
+    const user = await regularClientSchema.findOne({username})
+    console.log(user, "user")
+
+    if(!user){
+      res.status(401).json({
+        message:"The client token is not valid. Check that you have an account"
+      })
+    }
+  
+
+    req.authenticatedUser = {
+      ...req.authenticatedUser,
+      client:user
+    }
+
+    next()
+
+
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send("Invalid Token");
+  }
+}
+
+export default verifyJwt
---
+++ src/utils/validateFilter.js
@@ -0,0 +1,21 @@
+const isFlieAvaible = (vuelo,vueloTo ,body) => {
+
+  if(new Date(vuelo.aboarding).getUTCMonth() != new Date(vueloTo.since).getUTCMonth() ){
+    return false
+  }
+
+  if(!(new Date(vuelo.aboarding).getUTCDate() >= new Date(vueloTo.since).getUTCDate() && 
+      new Date(vuelo.aboarding).getUTCDate() <= new Date(vueloTo.to).getUTCDate())){
+    return false
+  }
+
+  if(!vuelo.destination.toUpperCase().includes(body.destination.toUpperCase()) ){
+    return false
+  }
+
+  return true
+}
+
+export default {
+  isFlieAvaible
+}
